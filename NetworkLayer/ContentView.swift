4871887014d5a88b0824aeeedbce94df3b66a904
Yung Dai <yungchidai@gmail.com>
2019-06-19 08:47:02 -0400

    Initial Commit

---
+++ NetworkLayer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NetworkLayer
+//
+//  Created by Yung Dai on 2019-06-19.
+//  Copyright © 2019 Yung Dai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
