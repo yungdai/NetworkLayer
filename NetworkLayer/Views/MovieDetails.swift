622a18db1ac0feacf218fd0fca61cf777064368e
Yung Dai <yungchidai@gmail.com>
2019-06-20 13:10:12 -0400

    Update MovieDetails.swift
    Add the ability only show the image when it's downloaded and fade into the MovieDetails

--- NetworkLayer/Views/MovieDetails.swift
+++ NetworkLayer/Views/MovieDetails.swift
@@ -13,25 +13,24 @@ struct MovieDetails : View {
 	let movie: Movie
 	
 	@State var image: Image = Image(systemName: "photo")
-	@State var imageIsHidden = true
+	@State var imageVisible = false
 	
 	var body: some View {
-
+		
 		GeometryReader { geometry in
 			
 			ZStack(alignment: .center) {
-
-				if self.imageIsHidden {
-					self.image.hidden()
-				} else {
+				
+				if self.imageVisible {
 					self.image
 						.resizable()
 						.blur(radius: 15, opaque: false)
 						.opacity(0.3)
+						.animation(.basic(duration: 0.3, curve: .easeInOut))
 				}
 
-
 				VStack(alignment: .leading, spacing: 20) {
+					Spacer()
 					HStack {
 						VStack(alignment: .leading) {
 							Text(self.movie.title)
@@ -41,11 +40,17 @@ struct MovieDetails : View {
 						}
 						
 						Spacer()
-						self.image
-							.resizable()
-							.frame(width: 60, height: 90)
-							.scaledToFit()
+						
+						if self.imageVisible {
+							self.image
+								.resizable()
+								.frame(width: 60, height: 90)
+								.scaledToFit()
+								.opacity(1)
+								.animation(.basic(duration: 0.3, curve: .easeInOut))
+						}
 					}
+					.frame(height: 100)
 					
 					
 					VStack(alignment: .leading) {
@@ -54,17 +59,19 @@ struct MovieDetails : View {
 						Text(self.movie.overview)
 							.font(.body)
 					}
-					}
-					.padding(.horizontal, 10.0)
-					.onAppear {
-						self.movie.getImageFrom(urlString: self.movie.posterPath) { image in
+					Spacer()
+				}
+				.padding(.horizontal, 10.0)
+				.onAppear {
+						
+					self.movie.getImageFrom(urlString: self.movie.posterPath)
+					{ image in
 							
-							self.image = image
-							self.imageIsHidden = false
-						}
+						self.image = image
+						self.imageVisible = true
+					}
 				}
 			}
-
 		}
 		.edgesIgnoringSafeArea(.all)
 	}
@@ -73,7 +80,7 @@ struct MovieDetails : View {
 #if DEBUG
 struct MovieDetails_Previews : PreviewProvider {
 	static var previews: some View {
-		MovieDetails(movie: testMovie, image: Image("black-panther"))
+		MovieDetails(movie: testMovie, image: Image("black-panther"), imageVisible: true)
 	}
 }
 #endif
